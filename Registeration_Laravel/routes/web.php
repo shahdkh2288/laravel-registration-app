1e5b943be343ba50e3850423d26946cf32725e90
Youssef <youssefkaramedu@gmail.com>
2024-05-11 21:00:18 +0300

    Remove /index from route

--- Registeration_Laravel/routes/web.php
+++ Registeration_Laravel/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/index', function () {
-  return view('index');
-});
+
 Route::get('/', function () {
     return view('welcome');
 });
